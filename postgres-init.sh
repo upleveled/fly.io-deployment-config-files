7cb6f4d945c062f37a2235797a7bc6a70dac5259
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-09-20 14:36:17 +0200

    Update script for deploy

--- postgres-init.sh
+++ postgres-init.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 
 if [ ! -d "../postgres-volume" ]
 then
@@ -17,4 +17,6 @@ else
   sed -i 's/run\/postgresql/postgres-volume\/run/g' /postgres-volume/data/postgresql.conf
   grep -qxF "listen_addresses='*'" /postgres-volume/data/postgresql.conf || echo "listen_addresses='*'" >> /postgres-volume/data/postgresql.conf
   [ ! -f /postgres-volume/data/postmaster.pid ] && su postgres -c "pg_ctl start -D /postgres-volume/data" || su postgres -c "pg_ctl restart -D /postgres-volume/data"
+  psql -U postgres postgres --command="CREATE USER $PGUSERNAME PASSWORD '$PGPASSWORD'" || echo "User already exists"
+  createdb -U postgres --owner=$PGUSERNAME $PGDATABASE || echo "Database already exists"
 fi
